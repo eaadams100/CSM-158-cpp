f7275d95957e574627ad5513f76f30eacd15b683
A. Emmanuel Adams <eaadams777@gmail.com>
2024-06-04 17:37:15 +0000

    Finding the sum of two numbers.

---
+++ text.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"ENTER VALUES FOR a AND b: \n";
+    cin>>a>>b;
+    cout<<"\n THE SUM OF a AND b IS = "<<a+b;
+    return 0;
+}
